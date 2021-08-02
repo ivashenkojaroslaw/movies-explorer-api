3ddb0918ca2c39cc08d799390034fa9cf0ec9e50
Yaroslav Ivashenko <yaroslav.@mail.ru>
2021-08-02 19:02:39 +0300

    fix: prosess request before controller

--- routes/signup.js
+++ routes/signup.js
@@ -4,7 +4,7 @@ const { createUser } = require('../controllers/user');
 
 router.post('/', celebrate({
   body: Joi.object().keys({
-    name: Joi.string().min(2).max(30),
+    name: Joi.string().required().min(2).max(30),
     email: Joi.string().required().email(),
     password: Joi.string().required().min(8),
   }),
